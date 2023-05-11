2662fc7f09f14b862316495cedb54a8986246fe3
mkorbar <matej.korbar@gmail.com>
2023-05-11 18:38:41 +0200

    Variables and conditionals (if statements)

---
+++ main.py
@@ -0,0 +1,53 @@
+
+# int
+whole_number = 42
+
+# float
+floating_point_number = 3.14
+
+# str
+normal_greeting = "Lep pozdrav!"
+
+# bool
+is_today_sunday = False
+is_it_raining = True
+
+# none
+empty_variable = None
+
+# pozdravljalnik
+# name = input("Vnesi svoje ime: ")
+# print("Lep pozdrav " + name + "!")
+
+
+# preprost seštevalnik
+# num1 = input("Vnesi prvi število: ")
+# num1 = float(num1)
+#
+# num2 = input("Vnesi drugo število: ")
+# num2 = int(num2)
+#
+# print(num1 + num2)
+
+
+# operacije za pogoje
+# primerjanje vrednosti
+# ==
+# !=
+# <, > <=, >=
+
+# if condition:
+#     # do this if condition is True
+#     print('')
+
+
+user_age = int(input("Vnesi svojo starost: "))
+
+if user_age > 18:
+    print('si polnoletna oseba')
+elif user_age < 5:
+    print('si otrok')
+else:
+    print("si mladoletna oseba")
+
+print('Program se v vsakem primeru nadaljuje tu')
