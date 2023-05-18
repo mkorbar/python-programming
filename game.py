23e676540cf932ad84e55a45974d9cca1f2e8c8f
mkorbar <matej.korbar@gmail.com>
2023-05-18 18:20:33 +0200

    Create the guess the number game

---
+++ game.py
@@ -0,0 +1,81 @@
+#
+# secret = 4
+#
+# guess = int(input("Vpiši skrito število: "))
+#
+# if secret == guess:
+#     print("Čestitke, uganil si pravilno število!")
+# else:
+#     print("Ups, to ne bo pravilno. Skrito število je " + str(secret))
+#
+
+
+# # while zanka
+# secret = 4
+# guess = None
+#
+# while guess != secret:
+#     guess = int(input("Vpiši skrito število: "))
+#
+#     if secret == guess:
+#         print("Čestitke, uganil si pravilno število!")
+#     else:
+#         print("Ups, to ne bo pravilno, poskusi znova.")
+#
+# print("Igra končana!")
+
+
+# # for zanka
+# secret = 4
+# guess = None
+#
+# print("Ugani število, imaš 5 poskusov.")
+# for trial_count in range(5):
+#     guess = int(input("Vpiši skrito število: "))
+#     print(f"to je tvoj {trial_count + 1}. poskus.")
+#
+#     if secret == guess:
+#         print("Čestitke, uganil si pravilno število!")
+#         break
+#     else:
+#         print("Ups, to ne bo pravilno. Poskusi ponovno.")
+#
+# print("Igra končana!")
+#
+# # if not (guess == secret):
+# if guess != secret:
+#     print(f"Pravilno število je bilo {secret}.")
+
+
+# While - break
+# secret = 4
+#
+# while True:
+#     guess = int(input("Vpiši skrito število: "))
+#
+#     if secret == guess:
+#         print("Čestitke, uganil si pravilno število!")
+#         break
+#     else:
+#         print("Ups, to ne bo pravilno, poskusi znova.")
+#
+# print("Igra končana!")
+#
+
+import random
+
+secret = random.randint(1, 30)
+
+while True:
+    guess = int(input("Vpiši skrito število: "))
+
+    if guess > secret:
+        print("Ne, poskusi z nižjim številom.")
+    elif guess < secret:
+        print("Ups, to ne bo pravilno, poskusi z višjim številom.")
+    else:
+        print("Čestitke, uganil si pravilno število!")
+        break
+
+print("Igra končana!")
+
